010b46aa4d893262b5f60fe4a26e48a22d087d44
Victor S. Ferreiras <VSFerreiras402@protonmail.com>
2025-03-10 17:27:15 -0400

    Inyectado contexto

--- Victor_Ferreiras_P2_AP1/Program.cs
+++ Victor_Ferreiras_P2_AP1/Program.cs
@@ -1,10 +1,16 @@
 using Victor_Ferreiras_P2_AP1.Components;
+using Victor_Ferreiras_P2_AP1.DAL;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
 
 var app = builder.Build();
 
