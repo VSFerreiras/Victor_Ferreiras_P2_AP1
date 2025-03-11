96f4740aa052de60287419a5efd77ec64a20ecf3
Victor S. Ferreiras <VSFerreiras402@protonmail.com>
2025-03-10 22:43:47 -0400

    Creado contexto

--- Victor_Ferreiras_P2_AP1/DAL/Contexto.cs
+++ Victor_Ferreiras_P2_AP1/DAL/Contexto.cs
@@ -5,5 +5,34 @@ namespace Victor_Ferreiras_P2_AP1.DAL;
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-    public DbSet<Modelo> Modelos { get; set; }
+
+    public virtual DbSet<Ciudades> Ciudades { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ciudades>().HasData(
+            new List<Ciudades>()
+            {
+                new()
+                {
+                    CiudadId = 1,
+                    CiudadNombre = "San Francisco",
+                    Monto = 20000,
+                },
+                new()
+                {
+                    CiudadId = 2,
+                    CiudadNombre = "Santiago",
+                    Monto = 650000,
+                },
+                new()
+                {
+                    CiudadId = 3,
+                    CiudadNombre = "Santo Domingo",
+                    Monto = 3500000,
+                }
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
 }
