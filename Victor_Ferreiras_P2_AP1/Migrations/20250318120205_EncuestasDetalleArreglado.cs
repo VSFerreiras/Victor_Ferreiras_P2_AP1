ab5b970f64e1a18ec1959f9ab7361b9f4ce4e845
Victor S. Ferreiras <vsferreiras402@protonmail.com>
2025-03-18 09:57:32 -0400

    Corregido EncuestasDetalles en migración

---
+++ Victor_Ferreiras_P2_AP1/Migrations/20250318120205_EncuestasDetalleArreglado.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Victor_Ferreiras_P2_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class EncuestasDetalleArreglado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
